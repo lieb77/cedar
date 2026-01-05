d78cfcfa694b5662db0e109163bb32c6961695c8
Paul Lieberman <lieb77@gmail.com>
2026-01-05 09:04:28 +0000

    Trying to add an image select to the settings form

--- src/Hook/CedarHooks.php
+++ src/Hook/CedarHooks.php
@@ -1,10 +1,11 @@
-<?php 
+<?php
 
 namespace Drupal\cedar\Hook;
 
 use Drupal\Core\Hook\Attribute\Hook;
 use Drupal\media\Entity\Media;
 use Drupal\file\Entity\File;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Modern ThemeHooks class for Drupal 11.3.
@@ -14,9 +15,9 @@ class CedarHooks {
 	* You can also use Attributes for standard hooks.
 	*/
 	#[Hook('preprocess_page')]
-	public function preprocessPage(array &$variables): void {		
+	public function preprocessPage(array &$variables): void {
 		$myConfigPage = \Drupal\config_pages\Entity\ConfigPages::config('site_settings');
-		$config = $myConfigPage->toArray();		
+		$config = $myConfigPage->toArray();
 		$media_id = $config['field_background_image'][0]['target_id'];
 		if ($media_id) {
 		    $media = Media::load($media_id);
@@ -24,12 +25,45 @@ class CedarHooks {
     				// Get the file entity from the media "image" field (usually named 'field_media_image')
 					$fid = $media->getSource()->getSourceFieldValue($media);
 					$file = File::load($fid);
-					
+
 					if ($file) {
 						$variables['site_bg_url'] = \Drupal::service('file_url_generator')
 					    ->generateString($file->getFileUri());
 					}
     			}
-		}		
+		}
 	}
+
+
+    /**
+     *
+     */
+    #[Hook('form_system_theme_settings_alter')]
+    public function formSystemThemeSettingsAlter(array &$form, FormStateInterface $form_state): void {
+
+        if ($form_state->getBuildInfo()['args'][0] == 'cedar') {
+           $form['header_image'] = [
+                '#type'  => "details",
+                '#title' => "Background Image",
+                '#open'  => true,
+                'settings' => [
+                    '#type' => 'container',
+                    'image_path' => [
+                        '#type' => 'textfield',
+                        '#title' => 'Path to image',
+                    ],
+                    'image_upload' => [
+                        '#type' => "file",
+                        '#title' => "Upload background image",
+                        '#upload_validators' => [
+                            'FileExtension' => [
+                                'extensions' => "png jpg jpeg",
+                            ],
+                        ],
+                    ],
+                ],
+            ];
+        }
+	}
+
 }
